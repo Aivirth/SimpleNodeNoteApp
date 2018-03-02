e04b50916db9973f6da1087c8195d82a27e58c4c
Aivirth <aivirth.work@gmail.com>
2018-03-02 11:50:50 +0100

    Added duplicate title entries filter and notes-data.json file creation

--- notes.js
+++ notes.js
@@ -1,5 +1,29 @@
+const fs = require('fs');
+
 const addNote = (title, body)=>{
-    console.log(`Adding note: ${title} , ${body}`);
+    let notes = [];
+    const note = {
+        title : title,
+        body : body
+    };
+
+    try {
+        let noteString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(noteString);
+    } catch (error) {
+        console.log(error)
+    }
+
+    let duplicateNotes = notes.filter(note => note.title === title);
+
+    if(duplicateNotes.length === 0){
+        notes.push(note);
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    }else{
+        console.log(`Note with title "${title}" already present`);
+    }
+    
+    
 }
 
 const getAll = () =>{
